21f6b0130184693bdce2e8325608da51e9d059f6
TƯỜNG HỮU <nguyenhuutuong999@gmail.com>
2019-04-26 22:15:03 +0700

    first commit

---
+++ help_center_Hiep/js/script.js
@@ -0,0 +1,31 @@
+// $(document).ready(function() {
+// //  console.log('hello');
+// //  window.alert('hello');
+
+
+// // $(*.btn*).click(function() {
+// // console.log('hello');
+// //   })
+// // $("#btnid").click(function(){
+// //     window.alert("hello");
+// // })
+// })
+
+
+$(document).ready(function() {
+    $(".tab ").click(function() {
+        $('.tab').removeClass("active");
+        $(this).addClass("active");
+       
+     if  ($(this).hasClass('tab-ques')) {
+         $('.tab-content').hide();
+         $('.content-ques').show();
+     } else if ($(this).hasClass('tab-des')) {
+         $('.tab-content').hide();
+         $('.content-des').show();
+     } else if ($(this).hasClass('tab-acc')) {
+         $('.tab-content').hide();
+         $('.content-acc').show();
+     } 
+  })
+})
